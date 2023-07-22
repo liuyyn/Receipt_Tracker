a9757da263b293de4de2821e39468f940dccbac6
Yu Yun Liu <yu.yun.liu@mail.mcgill.ca>
2023-07-22 17:40:58 -0400

    Initial Commit

---
+++ ReceiptTracker/ReceiptTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptTrackerApp.swift
+//  ReceiptTracker
+//
+//  Created by Yu Yun Liu on 2023-07-22.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
