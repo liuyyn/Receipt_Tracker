4bf07ee2e739419d445c60c79c7e2a45ebced551
Yu Yun Liu <yu.yun.liu@mail.mcgill.ca>
2023-09-21 18:09:12 -0400

    cleanup

--- backend/utils.py
+++ backend/utils.py
@@ -3,11 +3,14 @@ import base64
 from PIL import Image
 
 def compress_image(base64_str):
+    '''
+    this function takes a base64 string and returns a compressed base64 string and a png image
+    '''
     buffer = io.BytesIO()
     imgdata = base64.b64decode(base64_str)
     img = Image.open(io.BytesIO(imgdata))
     new_img = img.resize((400, 1000))  # x, y
     new_img.save(buffer, format="PNG")
-    new_img.save("test.png", format="PNG")
+    # new_img.save("test.png", format="PNG")
     img_b64 = base64.b64encode(buffer.getvalue())
     return {"compressed_img": str(img_b64)[2:-1], "png_image": buffer.getvalue()}
